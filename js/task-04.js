b2459315e6dfb442ce9043d2d4009b136672901a
Nadiia PRYNOVA <112952706+NadiiaPrynova@users.noreply.github.com>
2023-07-30 16:04:21 +0300

    update JS task 4, 7

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,16 @@
-const counterValue = document.querySelector("#value");
-counterValue.textContent = 0;
+const output = document.querySelector("#value");
+let counterValue = 0;
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-// console.log(counterValue);
-console.log(decrementBtn);
-// console.log(incrementBtn);
+
 
 incrementBtn.addEventListener('click', onIncrementBtnClick)
 function onIncrementBtnClick(event) {
-    counterValue.textContent += 1;
+    counterValue += 1;
+    output.textContent = counterValue;
 }
 decrementBtn.addEventListener('click', onDecrementBtnClick)
 function onDecrementBtnClick(event) {
-    counterValue.textContent -= 1;
+    counterValue -= 1;
+    output.textContent = counterValue;
 }
--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const refs = {
     inputControl: document.querySelector('#font-size-control'),
     text: document.querySelector('#text'),
 }
-
+refs.text.style.fontSize = `${refs.inputControl.value}px`;
 refs.inputControl.addEventListener('input', handlerInput);
 function handlerInput(event) {
     refs.text.style.fontSize = `${event.currentTarget.value}px`;
